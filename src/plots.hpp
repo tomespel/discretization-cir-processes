6617dee594ae4d9cf342d2bc3db91be54c1a7f61
Thomas J. Espel <thomas.espel@orange.fr>
2018-03-13 16:15:03 +0000

    Updated: report, plots.cpp

--- src/plots.hpp
+++ src/plots.hpp
@@ -5,4 +5,6 @@
 
 #include "cir2.hpp"
 
-void plot_paths_cir2(int n, int nn, double x, double T, double k, double a, double sigma, bool normal, string c);
+int plot_paths_cir2(int n, int nn, double x, double T, double k, double a, double sigma, bool normal, string c);
+
+int plot_paths_heston(int n, int nn, double x, double T, double k, double a, bool normal, string c);
